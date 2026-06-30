11fd560ed4490d05bc42a7298065184a3c358980
matinfuturity <jomiopu1201@gmail.com>
2026-06-30 15:11:46 +0900

    Remove test display code from script.js

--- live/script.js
+++ live/script.js
@@ -1,4 +1,3 @@
-// テスト表示（まずは動作確認）
 document.getElementById("race").textContent = "名古屋1R";
 document.getElementById("time").textContent = "発走 11:00";
 document.getElementById("safe").textContent = "◎5";
