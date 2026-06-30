d8937a0911c8fb8d5f33ab3d98b04b465e443a18
matinfuturity <jomiopu1201@gmail.com>
2026-06-30 15:11:32 +0900

    Update script.js

--- live/script.js
+++ live/script.js
@@ -1,1 +1,6 @@
-
+// テスト表示（まずは動作確認）
+document.getElementById("race").textContent = "名古屋1R";
+document.getElementById("time").textContent = "発走 11:00";
+document.getElementById("safe").textContent = "◎5";
+document.getElementById("longshot").textContent = "◎11";
+document.getElementById("MAJI").textContent = "◎8";
